5354d41bce11eedfad8d7514f6e28e68cc882bc3
thianhooi <thianhooi.ooi@appvantage.asia>
2023-09-21 16:01:26 +0800

    feat: join book author using loader

--- src/server/schema/resolvers/types/Book.ts
+++ src/server/schema/resolvers/types/Book.ts
@@ -2,6 +2,7 @@ import { GraphQLBookResolvers } from '../definitions';
 
 const BookGraphQL: GraphQLBookResolvers = {
     id: root => root._id,
+    author: (root, args, { loaders }) => loaders.userById.load(root.authorId),
 };
 
 export default BookGraphQL;
