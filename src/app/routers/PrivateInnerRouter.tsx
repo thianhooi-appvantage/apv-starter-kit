c26c8b16478c230d890c749d179c116fb431fee7
thianhooi <thianhooi.ooi@appvantage.asia>
2023-09-21 16:01:59 +0800

    feat: added router to book list page

--- src/app/routers/PrivateInnerRouter.tsx
+++ src/app/routers/PrivateInnerRouter.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import NotFoundResult from '../components/results/NotFoundResult';
 import ConsoleLayout from '../layouts/ConsoleLayout';
+import BookListPage from '../pages/private/BookListPage';
 import UserListPage from '../pages/private/UserListPage';
 import UserSelfPage from '../pages/private/UserSelfPage';
 
@@ -11,6 +12,7 @@ const PrivateInnerRouter = () => (
         <Routes>
             <Route element={<RedirectToHomePage />} path="" />
             <Route element={<UserListPage />} path="system/users" />
+            <Route element={<BookListPage />} path="system/books" />
             <Route element={<UserSelfPage />} path="self" />
             <Route element={<NotFoundResult />} path="*" />
         </Routes>
